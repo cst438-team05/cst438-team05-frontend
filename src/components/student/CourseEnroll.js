298c2eb1236e62f2a81ec703387a648cfb66093f
Chika Starks <chika.sue1@gmail.com>
2025-03-22 14:34:47 -0700

    updated CourseEnroll and ScheduleView (student)

--- src/components/student/CourseEnroll.js
+++ src/components/student/CourseEnroll.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
+import Button from "@mui/material/Button";
+import {SERVER_URL} from '../../Constants';
 
-// students displays a list of open sections for a 
+// students displays a list of open sections for a
 // use the URL /sections/open
 // the REST api returns a list of SectionDTO objects
 
@@ -9,12 +11,86 @@ import React, {useState, useEffect} from 'react';
 // studentId=3 will be removed in assignment 7.
 
 const CourseEnroll = (props) => {
-     
- 
-    return(
-        <>
-           <h3>Not implemented</h3>
-        </>
+    const [sections, setSections] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const fetchSections = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/sections/open`);
+
+            if (response.ok){
+                const sections = await response.json();
+                setSections(sections);
+                setMessage('');
+            } else {
+                const json = await response.json();
+                setMessage("Response error: " + json.message);
+            }
+        } catch (err) {
+            setMessage ("Unable to fetch sections: " + err);
+        }
+    };
+
+    const enrollAction = async (secNo) => {
+        try {
+            const response = await fetch (`${SERVER_URL}/enrollments/sections/${secNo}?studentId=3`,
+                {
+                    method: 'POST'
+                });
+            if (response.ok) {
+                setMessage(`Successfully enrolled in the section: ${secNo}`);
+            } else {
+                const json = await response.json();
+                setMessage("Enrollment error: " + json.message);
+            }
+        } catch (err) {
+            setMessage("Unable to enroll due to: " + err);
+        }
+    };
+
+    useEffect(() => {
+        fetchSections();
+    }, []);
+
+    return (
+        <div>
+            <h3>Course Enrollment</h3>
+            <h4>{message}</h4>
+            <table className="Center">
+                <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Semester</th>
+                    <th>Course ID</th>
+                    <th>Title</th>
+                    <th>Section ID</th>
+                    <th>Building</th>
+                    <th>Room</th>
+                    <th>Times</th>
+                    <th>Instructor Name</th>
+                    <th>Instructor Email</th>
+                    <th>Enroll</th>
+                </tr>
+                </thead>
+                <tbody>
+                {sections.map((s) => (
+                    <tr key={s.secNo}>
+                        <td>{s.year}</td>
+                        <td>{s.semester}</td>
+                        <td>{s.courseId}</td>
+                        <td>{s.title}</td>
+                        <td>{s.secId}</td>
+                        <td>{s.building}</td>
+                        <td>{s.room}</td>
+                        <td>{s.times}</td>
+                        <td>{s.instructorName}</td>
+                        <td>{s.instructorEmail}</td>
+                        <td><Button onClick={() => enrollAction(s.secNo)}> Enroll </Button></td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
