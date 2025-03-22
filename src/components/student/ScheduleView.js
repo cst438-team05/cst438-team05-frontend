5847ea75ff4fee51ba12617e61efd146c3944087
Chika Starks <chika.sue1@gmail.com>
2025-03-22 14:34:47 -0700

    updated CourseEnroll and ScheduleView (student)

--- src/components/student/ScheduleView.js
+++ src/components/student/ScheduleView.js
@@ -1,4 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import Button from '@mui/material/Button';
+import {SERVER_URL} from '../../Constants';
+import TextField from "@mui/material/TextField";
 
 // student can view schedule of sections 
 // use the URL /enrollments?studentId=3&year= &semester=
@@ -9,14 +12,118 @@ import React, {useState} from 'react';
 // issue a DELETE with URL /enrollments/{enrollmentId}
 
 const ScheduleView = (props) => {
-    
-   
-    return(
-        < > 
-            <h3>Not implemented</h3>
-        </ >
-    );
+    const headers = ["Section ID", "Title", "Credits", "Actions"];
+    const [schedules, setSchedules] = useState([]);
+    const [year, setYear] = useState('');
+    const [semester, setSemester] = useState('');
+    const [message, setMessage] = useState('');
 
+    const fetchSchedules = async () => {
+        if (!year || !semester){
+            setMessage("Please select year and semester");
+            return;
+        }
+        try {
+            const response = await fetch(`${SERVER_URL}/enrollments?studentId=3&year=${year}&semester=${semester}`);
+            if (response.ok) {
+                const data = await  response.json();
+                console.log("Fetched data:", data);
+                setSchedules(data);
+                setMessage('');
+            } else {
+                const json = await response.json();
+                setMessage("response error: " + json.message);
+            }
+        } catch (err) {
+            setMessage("network error: " + err);
+        }
+    };
+
+    const dropCourse = async (enrollmentId) => {
+        try {
+
+            //handling the grade constraint
+            const gradeResponse = await fetch(`${SERVER_URL}/enrollments/${enrollmentId}`);
+            if (!gradeResponse.ok) {
+                // If fetching grades failed, show the error
+                const errorData = await gradeResponse.json();
+                setMessage("Error fetching grades: " + errorData.message);
+                return;
+            }
+            const gradeData = await gradeResponse.json();
+            if (gradeData.length > 0) {
+                setMessage("Cannot delete enrollment. Grades exist for this enrollment.");
+                return;
+            }
+
+            const response = await fetch (`${SERVER_URL}/enrollments/${enrollmentId}`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+            if (response.ok) {
+                setMessage("Schedule deleted");
+                fetchSchedules();
+            } else {
+                const rc = await response.json();
+                setMessage(rc.message);
+            }
+        } catch (err) {
+            setMessage("network error: "+err);
+        }
+    };
+
+    useEffect( () => {
+        if (year && semester){
+            fetchSchedules();
+        }
+    }, [year, semester]);
+
+    return(
+        <div >
+            <h3>Schedules</h3>
+            <h4>{message}</h4>
+            <div>
+                <TextField
+                    label = "Year"
+                    name = "year"
+                    varian = "standard"
+                    required
+                    value={year}
+                    onChange={(e) => setYear(e.target.value)}>
+                </TextField>
+            </div>
+            <div>
+                <TextField
+                    label = "Semester"
+                    name = "semester"
+                    varian = "standard"
+                    required
+                    value={semester}
+                    onChange={(e) => setSemester(e.target.value)}>
+                </TextField>
+            </div>
+            <table className="Center">
+                <thead>
+                <tr>
+                    {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+                    </tr>
+                </thead>
+                <tbody>
+                {schedules.map((s) => (
+                    <tr key = {s.enrollmentId}>
+                        <td>{s.secId}</td>
+                        <td>{s.title}</td>
+                        <td>{s.credits}</td>
+                        <td><Button onClick = {() => dropCourse(s.enrollmentId)}> Delete </Button> </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div >
+    );
 }
 
 export default ScheduleView;
